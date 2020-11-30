49f6ba06b028e0baacd1999464bc986e5a17c485
dbxodyd1004 <66682820+dbxodyd1004@users.noreply.github.com>
2020-11-30 13:08:35 +0900

    v2 struct

--- go_struct_02.go
+++ go_struct_02.go
@@ -22,6 +22,10 @@ func defaultSubscriber(name string) subscriber {
 	return s
 }
 
+func applyDiscount(s *subscriber) {
+	s.rate = 4000
+}
+
 func main() {
 	s1 := defaultSubscriber("kim")
 	s1.rate = 4500
@@ -29,5 +33,9 @@ func main() {
 	
 	s2 := defaultSubscriber("park")
 	printInfo(s2)
+	
+	var s3 subscriber
+	applyDiscount(&s3)
+	fmt.Println(s3.rate)
 }
 
